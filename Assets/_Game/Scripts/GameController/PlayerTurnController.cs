20075f94ec4aaa74bd981f9dae6411acaefc75bc
boxcatdev <boxcatdev@gmail.com>
2024-03-07 09:23:29 -0600

    started player turn stuff

--- Assets/_Game/Scripts/GameController/PlayerTurnController.cs
+++ Assets/_Game/Scripts/GameController/PlayerTurnController.cs
@@ -4,8 +4,37 @@ using UnityEngine;
 
 public class PlayerTurnController : MonoBehaviour
 {
+    [SerializeField] private Transform _turnPanel;
+
     private void Start()
     {
         gameObject.SetActive(false);
+    }
+
+    public void ToggleAttackPanel(HexTile tile)
+    {
+        if (tile != null)
+        {
+            if (tile.TileType == TileType.City && tile.Team == GameTeam.Player)
+            {
+                //enable panel
+                //move panel to tile location
+                _turnPanel.gameObject.SetActive(true);
+
+                Vector3 uiPosition = Camera.main.WorldToScreenPoint(tile.transform.position);
+                _turnPanel.transform.position = uiPosition;
+
+                //disable selection
+                GridSelection.CanSelect = false;
+            }
+        }
+        else
+        {
+            //disable panel
+            _turnPanel.gameObject.SetActive(false);
+
+            //enable selection
+            GridSelection.CanSelect = true;
+        }
     }
 }
